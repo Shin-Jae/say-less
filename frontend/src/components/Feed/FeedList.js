5012599833b97fc7a69d7a8f7d229ea20117dfaa
Jae Shin <7jaeshin@gmail.com>
2022-05-08 12:50:48 -0400

    feed page css/started

--- frontend/src/components/Feed/FeedList.js
+++ frontend/src/components/Feed/FeedList.js
@@ -17,66 +17,72 @@ function FeedList() {
 
     return (
         <>
+
             <div className="feed-content">
-                <div className="question-form">
-                    <QuestionFormModal />
+                <div className="topic-content">
+                    <div>
+                        <h2>Topics</h2>
+                    </div>
+                    <div>
+                        <ul>
+                            <NavLink to='/business'>
+                                <div><li>Business</li></div>
+                            </NavLink >
+                            <div><li>Sports</li></div>
+                            <div><li>Entertainment</li></div>
+                            <div><li>Economics</li></div>
+                            <div><li>Music</li></div>
+                            <div><li>Art</li></div>
+                        </ul>
+                    </div>
                 </div>
-                <h2 className="ques-header">Questions</h2>
-                {!Object.values(questions).length && <span>No questions posted</span>}
-                <ul className="question-list">
-                    {Object.values(questions).map(({ id, question, description, User }) => {
-                        return <li className="q-list"><NavLink to={`/question/${id}`} key={id}>
-                            <div className="username">
-                                User(image): {User?.username}
-                            </div>
-                            <div className="question">
-                                Q: {question}
-                            </div>
-                            <div className="description">
-                                D: {description}
-                            </div>
-                        </NavLink>
-                            <span className="ques-btn">
-                                <NavLink to={`/home/${id}`}
-                                    className={`answer-btn-${id}`}
-                                >Answer
-                                </NavLink>
+                <div className="feed-list">
+                    <div className="question-form">
+                        <QuestionFormModal />
+                    </div>
+                    {!Object.values(questions).length && <span>No questions posted</span>}
+                    <ul className="question-list">
+                        {Object.values(questions).map(({ id, question, description, User }) => {
+                            return <li className="q-list"><NavLink to={`/question/${id}`} key={id}>
+                                <div className="username">
+                                    User(image): {User?.username}
+                                </div>
+                                <div className="question">
+                                    Q: {question}
+                                </div>
+                                <div className="description">
+                                    D: {description}
+                                </div>
+                            </NavLink>
+                                <span className="ques-btn">
+                                    <NavLink to={`/home/${id}`}
+                                        className={`answer-btn-${id}`}
+                                    >Answer
+                                    </NavLink>
 
-                                <button
-                                    className={`edit-btn-${id}`}
-                                >Edit
-                                </button>
-                                <button
-                                    className={`delete-btn-${id}`}
-                                >Delete
-                                </button>
+                                    <button
+                                        className={`edit-btn-${id}`}
+                                    >Edit
+                                    </button>
+                                    <button
+                                        className={`delete-btn-${id}`}
+                                    >Delete
+                                    </button>
 
-                            </span>
-                            <Switch>
-                                <Route path='/home/:id'>
-                                    <ViewAnswer />
-                                </Route>
-                            </Switch>
-                        </li>
+                                </span>
+                                <Switch>
+                                    <Route path='/home/:id'>
+                                        <ViewAnswer />
+                                    </Route>
+                                </Switch>
+                            </li>
 
-                    })}
+                        })}
 
 
-                </ul>
+                    </ul>
+                </div>
             </div>
-            <div className="topic">
-                <h2>Topics</h2>
-                <ul>
-                    <NavLink to='/business'>
-                        <li>Business</li>
-                    </NavLink >
-                    <li>Sports</li>
-                    <li>Movies</li>
-                    <li>Enviroment</li>
-                    <li>Video Games</li>
-                </ul>
-            </div>
-
         </>
     )
 }
